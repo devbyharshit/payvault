c623d4981516c189444724d6f70135017d43d55f
Harshit Anand <harshitanand283@gmail.com>
2025-04-12 20:55:09 +0530

    feat: code setup

---
+++ apps/bank_webhook_handler/src/index.ts
@@ -0,0 +1,25 @@
+import express from "express";
+import db from "@repo/db/client";
+
+const app = express();
+
+app.post("/hdfcWebhook", (req, res) => {
+	//TODO: Add zod validation here?
+	const paymentInformation = {
+		token: req.body.token,
+		userId: req.body.user_identifier,
+		amount: req.body.amount,
+	};
+
+	db.balance.update({
+		where: { userId: userId },
+		data: {
+			amount: { increment: paymentInformation.amount },
+		},
+	});
+
+	db.onRampTransaction.update({
+		where: { token: paymentInformation.token },
+		data: { status: "Success" },
+	});
+});
